8a342642f937552a7570de7de9b1e0361e5e4a33
unknown <emiloer97@gmail.com>
2022-04-07 11:01:13 +0200

    new files in repository

---
+++ js/slicksettings.js
@@ -0,0 +1,28 @@
+$('.team-carousel').slick({
+    
+    
+    arrows: false, 
+    autoplay: true,
+    autoplaySpeed: 2000,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2
+        }
+    },
+
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 3
+        }
+    }
+    ]
+    
+  }
+  
+  
+  );
